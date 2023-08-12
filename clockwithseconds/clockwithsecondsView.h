06e35c04801c7b2bb9d1c388f63ba61c06b56115
Caleb Gregory <calebgregory@gmail.com>
2023-08-12 13:28:32 -0400

    Initial Commit

---
+++ clockwithseconds/clockwithsecondsView.h
@@ -0,0 +1,12 @@
+//
+//  clockwithsecondsView.h
+//  clockwithseconds
+//
+//  Created by caleb on 8/12/23.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface clockwithsecondsView : ScreenSaverView
+
+@end
